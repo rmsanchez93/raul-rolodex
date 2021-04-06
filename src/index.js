d2fa67902408683fdd8db9ce659ba9de76423030
Raul Sanchez <raulsg93@gmail.com>
2021-04-06 17:16:34 -0500

    renamed for Lord of the Strings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    console.log('HTML is loaded!! good luck Texas SE-082420!')
+    console.log('HTML is loaded!! good luck Texas SE-022221!')
 })
